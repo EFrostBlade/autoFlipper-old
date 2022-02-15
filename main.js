5e2f4b205a59231db37da831f9b5567a165ff7f3
EFrostBlade <3171264475@qq.com>
2022-02-15 14:36:32 +0800

    Day1

--- main.js
+++ main.js
@@ -12,112 +12,84 @@ if (!$floaty.checkPermission()) {
 } 
 */
 Floaty();
-
 function Floaty() {
     window = floaty.rawWindow(
-        <horizontal gravity="center_vertical">
+        <vertical gravity="center_vertical">
             <img id="icon" src="file://res/icon.png" w="60" h="60" alpha="0.8" circle="true" borderWidth="1dp" borderColor="black" />
-            <horizontal id="list">
-                <vertical>
-                    <button id="ui_on" textColor="#FFFFFF" text="开始" bg="#4F4F4F" padding="0" h="40" w="60"/>
-                    <text text="" h="1" />
-                    <button id="ui_off" textColor="#FFFFFF" text="结束" bg="#4F4F4F" padding="0" h="40" w="60"/>
-                </vertical>
-            </horizontal>
-        </horizontal>
+            <vertical id="list">
+                <button id="start" textColor="#FFFFFF" text="开始" bg="#4F4F4F" padding="0" h="40" w="60" />
+                <text text="" h="1" />
+                <button id="exit" textColor="#FFFFFF" text="退出" bg="#4F4F4F" padding="0" h="40" w="60" />
+            </vertical>
+        </vertical>
     );
     window.setPosition(0, device.height / 3);
     window.exitOnClose();
-    setInterval(() => {}, 1000);
-
-    window.list.visibility = 8;
-
-    state = false;
+    setInterval(() => { }, 1000);
+    window.list.visibility = 8;//visibility属性决定了悬浮窗是否显示，8为隐藏，0为显示
+    state = false;//脚本运行状态
     var x = 0,
         y = 0;
     var windowX, windowY;
     var downTime;
-
-    window.icon.setOnTouchListener(function(view, event) {
+    window.icon.setOnTouchListener(function (view, event) {
         switch (event.getAction()) {
-            case event.ACTION_DOWN:
+            case event.ACTION_DOWN://监听到按下悬浮窗
                 x = event.getRawX();
-                y = event.getRawY();
+                y = event.getRawY();//获取点击坐标
                 windowX = window.getX();
-                windowY = window.getY();
-                downTime = new Date().getTime();
+                windowY = window.getY();//获取悬浮窗坐标
+                downTime = new Date().getTime();//获取按下时间
                 return true;
-            case event.ACTION_MOVE:
-                //window.setPosition(50, device.height / 3);
+            case event.ACTION_MOVE://监听移动悬浮窗                
                 window.setPosition(windowX + (event.getRawX() - x),
-                windowY + (event.getRawY() - y));
-                if(window.getX()<device.width / 2){
-                    window.setPosition(0,window.getY());
-                }else{
-                    window.setPosition(device.width-60,window.getY())
-                }
+                    windowY + (event.getRawY() - y));//随移动变化位置
+                if (window.getX() < device.width / 2) {
+                    window.setPosition(0, window.getY());
+                } else {
+                    window.setPosition(device.width - 60, window.getY())
+                }//贴边停靠
                 return true;
-            case event.ACTION_UP:
+            case event.ACTION_UP://监听弹起手指
                 if (Math.abs(event.getRawY() - y) < 5 && Math.abs(event.getRawX() - x) < 5) {
-                    liststate();
+                    liststate();//移动距离较小视为点击
                 }
                 return true;
         }
         return true;
     });
-
     function liststate() {
         if (window.list.visibility == 8) {
             window.list.visibility = 0;
         } else {
             window.list.visibility = 8;
-        }
+        }//点击时切换显示、隐藏
     }
-
-    window.ui_off.setOnTouchListener(function(view, event) {
-        if (event.getAction() == event.ACTION_UP) {
+    window.exit.setOnTouchListener(function (view, event) {
+        if (event.getAction() == event.ACTION_UP) {//点击退出按钮
             toastLog("关闭脚本...");
             window.close();
             exit();
         }
         return true;
     });
-
-    //on按钮事件
-    window.ui_on.setOnTouchListener(function(view, event) {
-        if (event.getAction() == event.ACTION_UP) {
-            window.setPosition(50, device.height / 3);
-            window.disableFocus();
-            if (window.ui_on.text() == "开始") {
-                window.ui_on.text("暂停");
-                console.log("开始on悬浮窗");
-
-                var main = threads.start(function() {
-                    device.keepScreenOn()
-                    //on脚本
-                    douyinTask.runDouYinTask();
-                })
-
-                //两秒不点击暂停，则隐藏list
-                setTimeout(function() {
-                    if (window.ui_on.text() == "暂停") {
-                        liststate()
+    window.start.setOnTouchListener(function (view, event) {
+        if (event.getAction() == event.ACTION_UP) {//点击开始/停止按钮
+            if (state==false) {//开始脚本
+                window.start.text("停止");//切换显示文字
+                toastLog("开始");
+                //此处预计添加脚本启动
+                state=true;
+                setTimeout(function () {
+                    if (window.start.text() == "停止") {
+                        liststate();
                     }
-                }, 3000)
-
-                //监控on还是暂停
-                var 监控state = setInterval(function() {
-                    if (window.ui_on.text() == "开始") { //是on说明暂停了
-                        main.interrupt()
-                        toastLog("暂停了")
-                        clearInterval(监控state)
-                    }
-                }, 100)
-
-            } else {
+                }, 3000)//三秒不点击停止，则隐藏列表       
+            } else {//停止脚本
                 state = false;
-                window.ui_on.text("开始");
-                toastLog("开始暂停...");
+                window.start.text("开始");
+                toastLog("停止");
+                //此处预计添加脚本停止
             }
         }
         return true;
